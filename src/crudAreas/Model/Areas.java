2b2956dcda0f34e7a687480e548dd42afdfe3085
Rafael Barbosa Camolese <rafael.camolese@qintess.com>
2024-12-16 12:55:34 +0000

    Update 8 files
    - /src/crudAreas/Model/Areas.java
    - /src/crudAreas/Service/AreasService.java
    - /src/crudAreas/Repository/AreasRepository.java
    - /src/crudAreas/areasApplication.java
    - /src/crudAreas/application.properties
    - /src/crudAreas/Controller/AreasController.java
    - /src/crudCompanhias/CompanhiasApplication.java
    - /src/crudCompanhias/application.properties

---
+++ src/crudAreas/Model/Areas.java
@@ -0,0 +1,27 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Areas {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String Name;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+}
---
+++ src/crudAreas/Repository/AreasRepository.java
@@ -0,0 +1,7 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Areas.Model.Areas;
+
+public interface AreasRepository extends JpaRepository<Areas, Long>{
+
+}
