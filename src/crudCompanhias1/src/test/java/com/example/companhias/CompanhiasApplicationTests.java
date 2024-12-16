6ee223ac491e4f8f12248b281cff3c14d977f526
re047194 <rafael.camolese@qintess.com>
2024-12-16 16:08:11 -0300

    https://gitlab.qintess.com/re047505/lgpd.git

---
+++ src/crudCompanhias1/src/test/java/com/example/companhias/CompanhiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companhias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanhiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
