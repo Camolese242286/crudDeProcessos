5c8396cc5ebe0fdb5d1be7cae8775eca527f27f3
Rafael Barbosa Camolese <rafael.camolese@qintess.com>
2024-12-16 15:14:17 +0000

    Update 5 files
    - /src/crudProcessos/test/Processos1ApplicationTests.java
    - /src/crudProcessos/.mvn/maven-wrapper.properties
    - /src/crudProcessos/.settings/org.eclipse.core.resources.prefs
    - /src/crudProcessos/.settings/org.eclipse.jdt.core.prefs
    - /src/crudProcessos/.settings/org.eclipse.m2e.core.prefs

---
+++ src/crudProcessos/test/Processos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Processos1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Processos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
