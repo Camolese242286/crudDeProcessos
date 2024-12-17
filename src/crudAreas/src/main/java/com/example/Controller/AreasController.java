42066aa55e61547f998a1d85734b9a781ec42d9d
Rafael Barbosa Camolese <rafael.camolese@qintess.com>
2024-12-17 18:49:46 +0000

    Update file AreasController.java

--- src/crudAreas/src/main/java/com/example/Controller/AreasController.java
+++ src/crudAreas/src/main/java/com/example/Controller/AreasController.java
@@ -2,7 +2,7 @@ package com.example.Controller;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +20,7 @@ import com.example.Areas.Service.AreasService;
 
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(value="areas")
 public class AreasController {
 
