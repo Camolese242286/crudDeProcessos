edd3734cdd2e18dfc45d01ce0b27d5b01058d155
Rafael Barbosa Camolese <rafael.camolese@qintess.com>
2024-12-18 14:10:56 +0000

    Update 6 files
    - /src/crudCompanhias/src/main/java/com/example/companhias/cors1/MyConfiguration.java
    - /src/crudCompanhias/src/main/java/com/example/companhias/cors/WebConfig.java
    - /src/crudAreas/src/main/java/com/example/cors/WebConfig.java
    - /src/crudAreas/src/main/java/com/example/cors1/MyConfiguration.java
    - /src/crudProcesos/src/main/java/com/example/cors/WebConfig.java
    - /src/crudProcesos/src/main/java/com/example/cors1/MyConfiguration.java

--- src/crudProcesos/src/main/java/com/example/cors1/MyConfiguration.java
+++ src/crudProcesos/src/main/java/com/example/cors1/MyConfiguration.java
@@ -6,7 +6,8 @@ public class MyConfiguration {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*").allowCredentials(true);
             }
         }
     }
