d8e306bf41e7f28dbda4d684523f788614e5ef5a
Rafael Barbosa Camolese <rafael.camolese@qintess.com>
2025-01-30 12:42:29 +0000

    Update 4 files
    - /src/Backend/BackendLGPD/Services/AreasService.java
    - /src/Backend/BackendLGPD/Controllers/AreasController.java
    - /src/Backend/BackendLGPD/Repositorys/CompanhiasRepository.java
    - /src/Backend/BackendLGPD/Repositorys/AreasRepository.java

---
+++ src/Backend/BackendLGPD/Repositorys/CompanhiasRepository.java
@@ -0,0 +1,9 @@
+package com.example.companhias.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.companhias.Model.Companhias;
+
+public interface CompanhiasRepository extends JpaRepository<Companhias, Long> {
+
+}
