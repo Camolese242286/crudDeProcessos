775a8387f284c93a81b1b8799b7928ac9a338424
re047194 <rafael.camolese@qintess.com>
2025-01-31 17:38:09 -0300

    incluindo o crudPerguntas

---
+++ FrontEnd/lgpd-dev/FrontEnd/lgpd-dev2/src/Backend/BackendPerguntas/src/main/java/com/example/Perguntas/PerguntasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Perguntas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerguntasApplication.class, args);
+	}
+
+}
